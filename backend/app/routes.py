b43af413663f76d80143a8541b98369b7f2500b8
s6l6m <salam.mokaid@gmail.com>
2024-11-28 13:00:47 +0100

    redirect index to docs

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
 
 from .api.rest import rest_router
 from .api.websocket import ws_router
 from .api.graphql import graphql_router
 
 router = APIRouter()
+
+@router.get("/")
+async def index():
+    return RedirectResponse(url="/docs")
 
 # REST API router
 router.include_router(rest_router, prefix="/rest")
