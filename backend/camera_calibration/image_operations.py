adbe861e769a5fbc095626dbda4a274d3934e530
lemu3947 <lea.mueller2@study.hs-duesseldorf.de>
2024-12-03 15:50:03 +0100

    kamera Kalibrierung

---
+++ backend/camera_calibration/image_operations.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+def calculate_image_metrics(image_path):
+    """
+    calculates brightness (mean), contrast (standard_devitaion) and sharpness (variance_of_laplacian) of the image
+
+    :param image_path: name of the image
+
+    return: mean, standard_devitatiion, variance_of_laplacian
+    """
+    image = cv2.imread(image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    mean = np.mean(gray)
+    standard_deviation = np.std(gray)
+    variance_of_laplacian = cv2.Laplacian(gray, cv2.CV_64F).var()
+    
+    return mean, standard_deviation, variance_of_laplacian
+
