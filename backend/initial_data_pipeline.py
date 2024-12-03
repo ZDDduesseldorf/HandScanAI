4ec8109ca945a34c9bb0ddc9813ddbce1e0f24d0
Marina Künzig <marina.kuenzig@study.hs-duesseldorf.de>
2024-12-03 22:24:28 +0100

    update initial data pipeline file

--- backend/initial_data_pipeline.py
+++ backend/initial_data_pipeline.py
@@ -1,20 +1,18 @@
-from validation.validation_pipeline import validation_pipeline, is_validation_pipeline_valid
-from datasets import ImagePathDataset,ImagePathWithCSVDataset
+import cv2
+import os
 from torch.utils.data import DataLoader
 
-import cv2
-
-from pipeline_utils import create_csv_with_header, add_entry_to_csv
-
 from app.utils import uuid
-import os
+from pipeline_utils import create_csv_with_header, add_entry_to_csv
+from datasets import ImagePathDataset, ImagePathWithCSVDataset, DatasetRegions
+from validation.validation_pipeline import validation_pipeline, is_validation_pipeline_valid
 
 
 ### This pipeline is for filtering 11K dataset
 
 # TODO: load images from 11K Dataset
 # dataloader
-folder_path = 'J:\Dokumente\MMI\HandScanAI\Repo\HandScanAI\TestImages' 
+folder_path = "J:\Dokumente\MMI\HandScanAI\Repo\HandScanAI\TestImages"
 dataset_11k = ImagePathWithCSVDataset(folder_path, csv_path="J:\Dokumente\MMI\HandScanAI\Repo\HandScanAI\HandInfo.csv")
 
 # TODO: Validate/ filter images
@@ -23,13 +21,18 @@ dataset_11k = ImagePathWithCSVDataset(folder_path, csv_path="J:\Dokumente\MMI\Ha
 # image path: UUID.pdf (see websocket)
 # result: base dataset
 csv_path = "CSV_filter.csv"
-csv_header = ["uuid","old_id","age","gender",]
+csv_header = [
+    "uuid",
+    "old_id",
+    "age",
+    "gender",
+]
 new_dataset_path = "NewDataset"
-os.makedirs(new_dataset_path, exist_ok=True) 
-create_csv_with_header(csv_path,csv_header)
+os.makedirs(new_dataset_path, exist_ok=True)
+create_csv_with_header(csv_path, csv_header)
 for image_paths, csv_data in dataset_11k:
     image = cv2.imread(image_paths)
-    vp =validation_pipeline(image)
+    vp = validation_pipeline(image)
     print(vp)
     vvp = is_validation_pipeline_valid(vp)
     print(vvp)
@@ -40,10 +43,10 @@ for image_paths, csv_data in dataset_11k:
         image_path = os.path.join(new_dataset_path, uuid + ".jpg")
         cv2.imwrite(image_path, image)
         ## save metadata at new_csv new row
-        csv_data['uuid'] =  uuid
-        csv_data['old_id'] = csv_data.pop('id')
+        csv_data["uuid"] = uuid
+        csv_data["old_id"] = csv_data.pop("id")
         print(csv_data)
-        add_entry_to_csv(csv_path,csv_data)
+        add_entry_to_csv(csv_path, csv_data)
         print("------------------------------------------------- one image done -----------")
         break
 
@@ -53,9 +56,18 @@ for image_paths, csv_data in dataset_11k:
 # provide Hand-regions as Enum or something similar to standardize path reading
 
 # TODO: embeddings
+print("--------------- Embeddings: Load dataset --------------------------------")
 # dataloader (loading with path)
+#path_to_images = #TODO (path to region images, see above)
+#dataset = DatasetRegions(path_to_region_images, clustered_data=True)
+
+print("--------------- Embeddings: Calculate embeddings --------------------------------")
 # calculate embeddings
-# provide embeddings per region to
+#for cluster in dataset:
+#    test_embeddings = embeddings_utils.calculate_embeddings_from_full_paths(cluster, models_utils.load_model())
+
+#    TODO: in same for-loop(?): provide embeddings per region to vector trees (see following)
+# TODO: or should embeddings be saved to be loaded into vector trees?
 
 # TODO: kNN (vector tree)
 # one kNN Tree per Hand-region
