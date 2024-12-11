23d3d6b85a2de72422c32fd750237626c7f3a942
Marina Künzig <marina.kuenzig@study.hs-duesseldorf.de>
2024-12-11 10:56:52 +0100

    refactor test

--- backend/tests/pipelines/test_initial_data_pipeline.py
+++ backend/tests/pipelines/test_initial_data_pipeline.py
@@ -1,19 +1,11 @@
-from pathlib import Path
-import os
-
 import pipelines.initial_data_pipeline as initial_pipeline
 
 
-def test_initial_data_pipeline():
-    # temp_base_dir geht bis backend
-    temp_base_dir = Path(__file__).resolve().parent.parent
-    print(temp_base_dir)
-    base_dataset_path = temp_base_dir / "data" / "TestBaseDataset"
-    region_dataset_path = temp_base_dir / "data" / "TestRegionDataset"
-
+def test_initial_data_pipeline(path_to_base_images, path_to_region_images):
+    # run initial data pipeline once and check that returned dict has len 2 (2 uuid clusters)
     embeddings = initial_pipeline.run_initial_data_pipeline(
-        base_dataset_path=base_dataset_path,
-        region_dataset_path=region_dataset_path,
+        base_dataset_path=path_to_base_images,
+        region_dataset_path=path_to_region_images,
         filter_initial_dataset=False,
         save_results_in_temp_folders=False,
     )
