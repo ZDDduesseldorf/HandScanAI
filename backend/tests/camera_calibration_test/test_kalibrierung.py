8b832e548096a4e4d0fa44eb47c9225cef3d8034
lemu3947 <lea.mueller2@study.hs-duesseldorf.de>
2024-12-12 08:40:00 +0100

    Verbesserung um Ruff-vorgaben zu entsprechen

--- backend/tests/camera_calibration_test/test_kalibrierung.py
+++ backend/tests/camera_calibration_test/test_kalibrierung.py
@@ -1,115 +1,131 @@
 import pytest
 import numpy as np
-import os
 
-from camera_calibration.kalibrierung import *
+import camera_calibration.kalibrierung as kali
+
 
 @pytest.fixture()  # `module` bedeutet, dass es einmal pro Testmodul ausgeführt wird
 def setup_method():
     image_path = "tests/data/calibration/kali_test.jpg"
     file_name = "tests/data/calibration/kalibrierungswerte.txt"
-    create_calibration_file(image_path, file_name)
+    kali.create_calibration_file(image_path, file_name)
     print("Setup abgeschlossen")
+
 
 def test_compare_mean_within_threshold():
     metrics = [100, 112, 98]
     k_values = [99, 101, 97]
-    assert compare_mean(metrics, k_values) == True
+    assert kali.compare_mean(metrics, k_values)
+
 
 def test_compare_mean_too_bright():
     metrics = [110, 112, 98]
     k_values = [99, 101, 97]
     with pytest.raises(ValueError, match="Bild ist zu hell"):
-       compare_mean(metrics, k_values)
+        kali.compare_mean(metrics, k_values)
+
 
 def test_compare_mean_too_dark():
     metrics = [90, 112, 98]
     k_values = [99, 101, 97]
     with pytest.raises(ValueError, match="Bild ist zu dunkel"):
-        compare_mean(metrics, k_values)
+        kali.compare_mean(metrics, k_values)
 
 
 def test_compare_std_within_threshold():
     metrics = [100, 102, 98]
     k_values = [99, 101, 97]
-    assert compare_std(metrics, k_values) == True
+    assert kali.compare_std(metrics, k_values)
+
 
 def test_compare_std_too_high():
     metrics = [100, 107, 98]
     k_values = [99, 101, 97]
     with pytest.raises(ValueError, match="Kontrast ist zu groß"):
-       compare_std(metrics, k_values)
+        kali.compare_std(metrics, k_values)
+
 
 def test_compare_std_too_low():
     metrics = [100, 95, 98]
     k_values = [99, 101, 97]
     with pytest.raises(ValueError, match="Kontrast ist zu klein"):
-        compare_std(metrics, k_values)
+        kali.compare_std(metrics, k_values)
 
 
 def test_compare_sharpness_within_threshold():
     metrics = [100, 102, 10]
     k_values = [99, 101, 9]
-    assert compare_sharpness(metrics, k_values) == True
+    assert kali.compare_sharpness(metrics, k_values)
+
 
 def test_compare_sharpness_too_low():
     metrics = [100, 112, 7]
     k_values = [99, 101, 9]
     with pytest.raises(ValueError, match="Bild ist zu unscharf"):
-        compare_sharpness(metrics, k_values)
+        kali.compare_sharpness(metrics, k_values)
 
 
 def test_compare_rectangle_identical():
-    rectangle = [[1009, 1130]],[[1093, 1347]], [[1209, 1183]], [[1234, 1391]]
+    rectangle = [[1009, 1130]], [[1093, 1347]], [[1209, 1183]], [[1234, 1391]]
     rectangle = np.array(rectangle)
-    k_values = [99, 101, 9, 760, [[1039, 1170],[1063, 1367], [1219, 1153], [1244, 1351]]]
-    assert compare_rectangle(rectangle, k_values) == True
+    k_values = [99, 101, 9, 760, [[1039, 1170], [1063, 1367], [1219, 1153], [1244, 1351]]]
+    assert kali.compare_rectangle(rectangle, k_values)
+
 
 def test_compare_rectangle_not_identical():
-    rectangle = [[909, 1130]],[[1093, 1347]], [[1209, 1183]], [[1234, 1391]]
+    rectangle = [[909, 1130]], [[1093, 1347]], [[1209, 1183]], [[1234, 1391]]
     rectangle = np.array(rectangle)
-    k_values = [99, 101, 9, 760, [[1039, 1170],[1063, 1367], [1219, 1153], [1244, 1351]]]
+    k_values = [99, 101, 9, 760, [[1039, 1170], [1063, 1367], [1219, 1153], [1244, 1351]]]
     with pytest.raises(ValueError, match=r"Rechtecke nicht identisch bei Punkt \[ *909 1130\]"):
-        compare_rectangle(rectangle, k_values)
+        kali.compare_rectangle(rectangle, k_values)
 
 
 def test_compare_extensive_within_threshold():
-    rectangle = [[0, 0],[0, 1], [1, 1], [1, 0]]
+    rectangle = [[0, 0], [0, 1], [1, 1], [1, 0]]
     rectangle = np.array(rectangle)
-    k_values = [99, 101, 9, 4, [[0, 0],[0, 1], [1, 1], [1, 0]]]
-    assert compare_extensive(rectangle, k_values) == True
+    k_values = [99, 101, 9, 4, [[0, 0], [0, 1], [1, 1], [1, 0]]]
+    assert kali.compare_extensive(rectangle, k_values)
+
 
 def test_compare_extensive_too_high():
-    rectangle = [[0, 0],[0, 2], [2, 2], [2, 0]]
+    rectangle = [[0, 0], [0, 2], [2, 2], [2, 0]]
     rectangle = np.array(rectangle)
-    k_values = [99, 101, 9, 4, [[0, 0],[0, 1], [1, 1], [1, 0]]]
+    k_values = [99, 101, 9, 4, [[0, 0], [0, 1], [1, 1], [1, 0]]]
     with pytest.raises(ValueError, match="Rechteck zu groß"):
-       compare_extensive(rectangle, k_values)
+        kali.compare_extensive(rectangle, k_values)
+
 
 def test_compare_extensive_too_low():
-    rectangle = [[0, 0],[0, 0.5], [0.5,0.5], [0.5, 0]]
+    rectangle = [[0, 0], [0, 0.5], [0.5, 0.5], [0.5, 0]]
     rectangle = np.array(rectangle)
-    k_values = [99, 101, 9, 4, [[0, 0],[0, 1], [1, 1], [1, 0]]]
+    k_values = [99, 101, 9, 4, [[0, 0], [0, 1], [1, 1], [1, 0]]]
     with pytest.raises(ValueError, match="Rechteck zu klein"):
-        compare_extensive(rectangle, k_values)
+        kali.compare_extensive(rectangle, k_values)
 
 
 def test_check_kali(setup_method):
     image_path = "tests/data/calibration/test_bild.jpg"
     file_name = "tests/data/calibration/kalibrierungswerte.txt"
-    assert check_kalibration(image_path, file_name) == True
+    assert kali.check_kalibration(image_path, file_name)
+
 
 def test_check_kali_failes(setup_method):
     image_path = "tests/data/calibration/failed_kali.jpg"
     file_name = "tests/data/calibration/kalibrierungswerte.txt"
-    with pytest.raises(ValueError): 
-        check_kalibration(image_path, file_name) == False
+    with pytest.raises(ValueError):
+        kali.check_kalibration(image_path, file_name)
 
 
 def test_read_calibration_file(setup_method):
     file_name = "tests/data/calibration/kalibrierungswerte.txt"
-    k_values = read_calibrationfile(file_name)
-    expected_values = (250.616159456983, 20.653294995518543,0.8325489485543115, 996.1001768764597, [[1913, 995], [1917, 1246], [2160, 992], [2164, 1243]])
+    k_values = kali.read_calibrationfile(file_name)
+    expected_values = (
+        250.616159456983,
+        20.653294995518543,
+        0.8325489485543115,
+        996.1001768764597,
+        [[1913, 995], [1917, 1246], [2160, 992], [2164, 1243]],
+    )
     print(k_values)
     # Vergleiche die numerischen Werte zuerst
     for i in range(4):
