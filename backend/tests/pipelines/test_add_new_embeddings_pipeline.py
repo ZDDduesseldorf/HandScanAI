088b1cc4021f6671bd1fb276d0e9a02779ec47c3
leamueller06 <lea.mueller0609@gmail.com>
2025-01-10 19:03:55 +0100

    Hinzufügen embedding zu csv in pipeline

--- backend/tests/pipelines/test_add_new_embeddings_pipeline.py
+++ backend/tests/pipelines/test_add_new_embeddings_pipeline.py
@@ -3,5 +3,5 @@ from pipelines.add_new_embeddings_pipeline import run_add_new_embeddings_pipelin
 
 def test_add_new_embeddings_pipeline():
     uuid = "514f53d0-6aab-4da1-b929-8f1dc0817289"
-    dict_embedding = run_add_new_embeddings_pipeline(uuid, testing=True)
-    assert len(dict_embedding) == 7
+    saved = run_add_new_embeddings_pipeline(uuid, testing=True)
+    assert saved
