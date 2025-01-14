281570560e07afbe2493c99027060be141c6d33b
Voxel007 <alexander.va245@gmail.com>
2025-01-14 12:16:54 +0100

    added another button component since ther routebuttoncomponent is locked to a position
    in the furute I will look into how to split the components more efficiently

---
+++ frontend/src/components/GeneralButton.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Button from '@mui/material/Button';
+import styled from '@mui/material/styles/styled';
+
+const CustomButton = styled(Button)({
+  backgroundColor: '#0F3EB5',
+  borderRadius: 0,
+  variant: 'contained',
+  color: 'white',
+  disableElevation: true,
+  fontFamily: 'Delius Unicase',
+});
+
+interface Props {
+  children: ReactNode;
+  RouteTo: string;
+}
+
+const GeneralButton: React.FC<Props> = ({ children, RouteTo }) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(RouteTo);
+  };
+
+  return (
+    // JSX for your component goes here
+    <CustomButton onClick={handleClick}>{children}</CustomButton>
+  );
+};
+
+export default GeneralButton;
--- frontend/src/pages/Explanation.tsx
+++ frontend/src/pages/Explanation.tsx
@@ -3,55 +3,65 @@ import { Box, Button, styled, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import StyledTitle from '@/styles/StyledTitle';
 import NavButton from '@/components/NavButton';
+import GeneralButton from '@/components/GeneralButton';
 
 const BoxText = styled(Typography)`
   font-family: 'Poppins', sans-serif;
   margin: 0 0 1 rem;
   text-align: justify;
-  font-size: 0.8rem;
-  color: #1a3ab8;
+  font-size: 1.2rem;
+  color: #000000;
 `;
 const SecondaryHeading = styled(Typography)`
   font-family: 'Delius Unicase', cursive;
-  margin: 0 0 0.5rem;
+  // margin: 0 0 0.5rem;
+  margin-top: 1.5rem;
+
   text-align: left;
   font-size: clamp(1rem, 2vw, 1.5rem);
-  color: #1a3ab8;
+  color: #000000;
 `;
-const MiddlePageLayout = styled(Typography)`
-  display: grid;
+const HandGalleryLayout = styled(Typography)`
+  display: flex;
+  flex-direction: row;
+`;
+const HandImgLayout = styled(Typography)`
+  display: flex;
+  flex-direction: column;
 `;
 
 const Explanation: React.FC = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   return (
     <Box>
       <StyledTitle>Erklärung</StyledTitle>
       <SecondaryHeading>
-        Hier siehst du dein Bild und drei weitere die vom alter und Geschlcht
-        doch gut zu dinEm passen sollten, oder?
+        Hier siehst du dein Bild und drei weitere die vom alter und Geschlecht
+        doch gut zu deinem passen sollten, oder?
       </SecondaryHeading>
       <BoxText>
         Hand Scan AI nutzt diese Ähnlichkeiten, um dir eine Vorhersage zu geben,
         basierend auf den Bildern, die am meisten mit deinem Bild übereinstimmen
       </BoxText>
       <div></div>
-      <SecondaryHeading>
+      <SecondaryHeading style={{ textAlign: 'center' }}>
         Statt Hand Scan AI kannst du auch k-NN Algorithmus sagen
       </SecondaryHeading>
-      <BoxText>
+      <BoxText style={{ textAlign: 'center' }}>
         „k-Nearest Neighbors (k-NN)“ ist eine Methode des maschinellen Lernens,
         die auf der Annahme basiert, dass ähnliche Bilder ähnliche Ergebnisse
         liefern. Nachfolgend die wichtigsten Schritte:
       </BoxText>
-      <MiddlePageLayout></MiddlePageLayout>
+      <HandGalleryLayout></HandGalleryLayout>
       <SecondaryHeading>Mehr zur KI</SecondaryHeading>
       <BoxText>
         Klicke hier, um mehr über die „Black Box KI“ zu erfahren und den
         zugrunde liegenden Algorithmus zu entdecken und Fachbegriffe
         wie Embeddings kennenzulernen, die für die Vorhersagen wichtig sind.
       </BoxText>
+      <GeneralButton RouteTo="/results-age">Erfahre mehr über Ki</GeneralButton>
+
       <SecondaryHeading>
         Körperliche Merkmale und ihre Bedeutung
       </SecondaryHeading>
@@ -61,14 +71,7 @@ const Explanation: React.FC = () => {
         dabei eine Rolle spielen und wie diese in die Vorhersage einfließen.
       </BoxText>
 
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => navigate('/blackbox')}
-      >
-        Erklärung
-      </Button>
-      {/* <NavButton>Erfahre mehr über Ki</NavButton> */}
+      <GeneralButton RouteTo="/blackbox">Erklärung</GeneralButton>
       <NavButton RouteTo="/results-gender">Weiter</NavButton>
     </Box>
   );
