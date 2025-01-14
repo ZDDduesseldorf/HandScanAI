94eb454f09fc09d4e133749fe11f086f5e4352b1
s6l6m <salam.mokaid@gmail.com>
2025-01-14 12:16:20 +0100

    close and flush db functions

--- backend/app/db/config.py
+++ backend/app/db/config.py
@@ -2,11 +2,24 @@ import motor.motor_asyncio
 from beanie import init_beanie
 
 from app.core.config import settings
-from app.db.models import TestModel
+from app.db.models import ScanEntry
+
+client = motor.motor_asyncio.AsyncIOMotorClient(settings.MONGO_URI)
 
 
 async def init_db() -> None:
-    """ Initialize the database and create the necessary models """
+    """Initialize the database and create the necessary models"""
 
-    client = motor.motor_asyncio.AsyncIOMotorClient(settings.MONGO_URI)
-    await init_beanie(database=client.db_name, document_models=[TestModel])
+    await init_beanie(database=client.db_name, document_models=[ScanEntry])
+
+
+async def close_db() -> None:
+    """Close the database connection"""
+
+    client.close()
+
+
+async def flush_db() -> None:
+    """Flush the database"""
+
+    await client.drop_database(client.db_name)
