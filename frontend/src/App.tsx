4ab1d67eff10252b50514bbb0b4f5d9676a47fd4
Voxel007 <alexander.va245@gmail.com>
2025-01-16 14:04:35 +0100

    Refactored Apollo-client code into Apollo-clietn.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,34 +18,9 @@ import Layout from '@/components/Layout';
 
 import '@/assets/fonts.css';
 import '@/App.css';
-
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  HttpLink,
-  from,
-} from '@apollo/client';
-import { onError } from '@apollo/client/link/error';
+import client from './GraphQL/apollo-client'; // Import the client from Apollo-client.tsx
 import GetModels from './components/TestGraphQL';
-
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors) {
-    graphQLErrors.map(({ message, locations, path }) => {
-      alert(`Graphql error ${message}`);
-    });
-  }
-});
-
-const link = from([
-  errorLink,
-  new HttpLink({ uri: 'http://localhost:8000/graphql' }),
-]);
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: link, //according to documentation I could also put uri with graphql endpoint here
-});
+import { ApolloProvider } from '@apollo/client';
 
 const App: React.FC = () => {
   const location = useLocation();
---
+++ frontend/src/GraphQL/Apollo-client.tsx
@@ -0,0 +1,23 @@
+import { ApolloClient, InMemoryCache, HttpLink, from } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+//   import GetModels from './components/TestGraphQL';
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: 'http://localhost:8000/graphql' }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link, //according to documentation I could also put uri with graphql endpoint here
+});
+
+export default client;
