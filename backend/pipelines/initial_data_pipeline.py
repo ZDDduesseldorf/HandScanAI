c1a0c2e3f037f366617b6e652179f8cfb96a02d1
leamueller06 <lea.mueller0609@gmail.com>
2025-01-23 15:13:08 +0100

    Testszenarien für Embeddings, Hinzufügen flags in initial_data_pipeline, Prüfung file/folder exists

--- backend/pipelines/initial_data_pipeline.py
+++ backend/pipelines/initial_data_pipeline.py
@@ -1,32 +1,43 @@
 from .datasets import ImagePathDataset, DatasetRegionClusters
-from embeddings.embeddings_utils import calculate_embeddings_from_path_dict
+from embeddings.embeddings_utils import calculate_embeddings_from_path_dict, _default_cnn_model_
 import hand_normalization.src.main as normalization
 from .regions_utils import PipelineDictKeys as Keys
 from .csv_utils import create_region_csvs, add_embedding_dict_to_csv
 
 
 ### This pipeline is for filtering 11K dataset
-def run_initial_data_pipeline(base_dataset_path, region_dataset_path, csv_folder_path, save_results=True):
+def run_initial_data_pipeline(
+    base_dataset_path,
+    region_dataset_path,
+    csv_folder_path,
+    model=_default_cnn_model_,
+    normalize=False,
+    save_images=True,
+    save_csvs=True,
+):
     ######## STEP 1: Hand normalization #################################
-    print("--------------- Hand-Normalization: Load dataset --------------------------------")
-    # dataloader
-    dataset_base = ImagePathDataset(base_dataset_path)
+    if normalize:
+        print("--------------- Hand-Normalization: Load dataset --------------------------------")
+        # dataloader
 
-    print("--------------- Hand-Normalization: Normalize Images --------------------------------")
-    # normalize images
-    for path_dict in dataset_base:
-        uuid = path_dict[Keys.UUID.value]
-        path = path_dict[Keys.IMAGE_PATH.value]
-        regions_dict = normalization.normalize_hand_image(path)
+        dataset_base = ImagePathDataset(base_dataset_path)
 
-        # save normalized images (path: UUID_HandRegion)
-        # for tests and debugs possible to comment this out
-        if save_results:
-            normalization.save_region_images(uuid, regions_dict, region_dataset_path)
+        print("--------------- Hand-Normalization: Normalize Images --------------------------------")
+        # normalize images
+
+        for path_dict in dataset_base:
+            uuid = path_dict[Keys.UUID.value]
+            path = path_dict[Keys.IMAGE_PATH.value]
+            regions_dict = normalization.normalize_hand_image(path)
+
+            # save normalized images (path: UUID_HandRegion)
+            # for tests and debugs possible to comment this out
+            if save_images:
+                normalization.save_region_images(uuid, regions_dict, region_dataset_path)
 
     ######## STEP 2: Embeddings #################################
-    # TODO: löschen nach Implementierung Vektordatenbank
-    if save_results:
+    # relevant für Test-Szenarien
+    if save_csvs:
         create_region_csvs(csv_folder_path)
 
     print("--------------- Embeddings: Load dataset --------------------------------")
@@ -40,11 +51,12 @@ def run_initial_data_pipeline(base_dataset_path, region_dataset_path, csv_folder
     # cluster is a dict {'uuid': str, 'image_paths': dict {HandRegions.key: path (str)}}
     for image_path_regions_cluster in dataset:
         embeddings_regions_dict = calculate_embeddings_from_path_dict(
-            image_path_regions_cluster[Keys.IMAGE_PATHS_INITIAL.value]
+            image_path_regions_cluster[Keys.IMAGE_PATHS_INITIAL.value], model
         )
         added_embedding = False
-        if save_results:
-            # TODO: ersetzen durch Vektordatenbank
+        uuid = image_path_regions_cluster[Keys.UUID.value]
+        if save_csvs:
+            # relevant für Test-Szenarien
             added_embedding = add_embedding_dict_to_csv(csv_folder_path, uuid, embeddings_regions_dict)
 
         # ab hier für Unit-Tests
--- backend/tests/scenario_embeddings/distance_pipelines.py
+++ backend/tests/scenario_embeddings/distance_pipelines.py
@@ -5,34 +5,92 @@ import hand_normalization.src.main as normalization
 from pipelines.data_utils import build_info_knn
 from pipelines.distance_calculation import calculate_distance
 from pipelines.inference_pipeline import _path_manager, get_image_path
+from utils.logging_utils import save_nearest_neighbours_info
+from pipelines.csv_utils import check_or_create_folder, check_file_exists, create_csv_with_header
+from embeddings.models_utils import CNNModel, load_model
+from pipelines.initial_data_pipeline import run_initial_data_pipeline
+from pipelines.regions_utils import PipelineDictKeys as Keys
+
+
+# TODO: zum Ausführen der distance_pipeline verwenden
+"""def test_scenario_embeddings():
+    run_scenarios_embeddings(setup=False)"""
+
+
+def scenario_path_manager():
+    scenario_embeddings_path = Path(__file__).resolve().parent
+    path_to_result_csv = scenario_embeddings_path / "result_csvs"
+
+    return path_to_result_csv
+
+
+def setup_scenario_structure(path_to_model_folder, model):
+    check_or_create_folder(path_to_model_folder)
+    _, folder_path_region, _, _, folder_path_base = _path_manager(testing=False)
+    run_initial_data_pipeline(folder_path_base, folder_path_region, path_to_model_folder, model, save_images=False)
+
+
+def check_or_create_nearest_neighbours_csv(path_to_csv_file):
+    if check_file_exists(path_to_csv_file):
+        return True
+    else:
+        header_nearest_neigbour = [
+            Keys.UUID.value,
+            Keys.REGION.value,
+            Keys.NEIGHBOUR_UUID.value,
+            Keys.DISTANCE.value,
+            Keys.AGE.value,
+            Keys.GENDER.value,
+        ]
+        create_csv_with_header(path_to_csv_file, header_nearest_neigbour)
+
 
 # TODO: embeddings_csvs je Model erstellen
-def run_scenarios_embeddings():
+def run_scenarios_embeddings(setup=False):
+    path_to_result_csv = scenario_path_manager()
+    models_dict = {
+        "DENSENET_121": load_model(CNNModel.DENSENET_121),
+        "DENSENET_169": load_model(CNNModel.DENSENET_169),
+        "RESNET_50": load_model(CNNModel.RESNET_50),
+    }
+    k = 10
+    uuid_list = ["514f53d0-6aab-4da1-b929-8f1dc0817289", "3ffd5331-da5d-4818-8af7-44869413714c"]
+    for model_name, model in models_dict.items():
+        if setup:
+            model_csv_path = path_to_result_csv / model_name
+            setup_scenario_structure(model_csv_path, model)
+        for uuid in uuid_list:
+            run_distance_pipeline(uuid, model_name, model, k)
+    # for
+    # run_distance_pipeline()
     # uuids der QueryBilder (bereits vorhanden, linke/rechte Hand in Datensatz vorhanden) 6-7 personen
     # 3 verschiedene modelle
     # k=10
 
-    # wie vergleicht man die Ergebnisse am besten? niedrige Distanz nicht zwangsläufig gutes Ergebnis? 
+    # wie vergleicht man die Ergebnisse am besten? niedrige Distanz nicht zwangsläufig gutes Ergebnis?
     # -> Was ist gutes Ergebnis? (ähnliche Bild einer Person sollte ähnliches Embedding liefern)
     # von machen Personen viele Bilder drin von anderen weniger
     # These 1: nur 2 Bilder einer Person -> 1. Distanz 0, 2. anderes Bild
     # These 2: Augmentated Bilder -> Bilder der selben person am nächsten
     # alle Variablen hängen voneinander ab? Gridsearch?
 
-    
-def run_scenarios_classfiers():
-    # uuids der QueryBilder (11k, eigene Bilder)
-    # festgelegtes model
-    # verschiedene ks (3,5,7,10)
-    # verschiedene distanzmethoden ? (cosinus)
-    # verschiedene Classfier pro Region (simple(mean, modus), gewichtung nach Distanz, Random Forest)
-    # Ensemble Classifier (simple(mean, modus), Gewichtung nach Region)
-    # Vergleich mit erwartetem Wert (Alter, Geschlecht)
+
+# def run_scenarios_classfiers():
+# uuids der QueryBilder (11k, eigene Bilder)
+# festgelegtes model
+# verschiedene ks (3,5,7,10)
+# verschiedene distanzmethoden ? (cosinus)
+# verschiedene Classfier pro Region (simple(mean, modus), gewichtung nach Distanz, Random Forest)
+# Ensemble Classifier (simple(mean, modus), Gewichtung nach Region)
+# Vergleich mit erwartetem Wert (Alter, Geschlecht)
 
 
-def run_distance_pipeline(uuid, model, k):
-    folder_path_query, _, embedding_csv_path, metadata_csv_path = _path_manager(testing=False)
+def run_distance_pipeline(uuid, model_name, model, k, save_results=True):
+    # produktiv Daten aus Media Ordner
+    folder_path_query, _, _, metadata_csv_path, _ = _path_manager(testing=False)
+    path_to_results_csv = scenario_path_manager()
 
+    model_embedding_csv_path = path_to_results_csv / model_name
     ######## STEP 0: build path to image #################################
 
     image_path = get_image_path(folder_path_query, uuid)
@@ -48,12 +106,12 @@ def run_distance_pipeline(uuid, model, k):
 
     ######## STEP 3: search nearest neighbours ###########################
 
-    dict_all_dist = calculate_distance(dict_embedding, k, embedding_csv_path)
+    dict_all_dist = calculate_distance(dict_embedding, k, model_embedding_csv_path)
 
     dict_all_info_knn = build_info_knn(metadata_csv_path, dict_all_dist)
 
-
+    if save_results:
+        nearest_neighbour_csv_path = model_embedding_csv_path / "nearest_neighbours.csv"
+        check_or_create_nearest_neighbours_csv(nearest_neighbour_csv_path)
+        save_nearest_neighbours_info(uuid, dict_all_info_knn, nearest_neighbour_csv_path)
     # TODO: Abspeicherung des dicts + metadaten in cvs oder alternativen
-
-
-
