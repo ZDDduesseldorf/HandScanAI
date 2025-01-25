04fd2de9414347894dd086d4762112d06bff9d37
Marina Künzig <marina.kuenzig@study.hs-duesseldorf.de>
2025-01-25 14:40:02 +0100

    added docstring to initial data pipeline

--- backend/pipelines/initial_data_pipeline.py
+++ backend/pipelines/initial_data_pipeline.py
@@ -5,7 +5,6 @@ from .regions_utils import PipelineDictKeys as Keys
 from .csv_utils import create_region_csvs, add_embedding_dict_to_csv
 
 
-### This pipeline is for filtering 11K dataset
 def run_initial_data_pipeline(
     base_dataset_path,
     region_dataset_path,
@@ -15,6 +14,25 @@ def run_initial_data_pipeline(
     save_images=True,
     save_csvs=True,
 ):
+    """
+    This Funktion
+    - takes a base dataset (containing images and the corresponding metadata as csv),
+    - normalized the images into hand-region-images and saves them as bitmaps,
+    - generates the images' embeddings and saves them per region (in csv-files).
+
+    For testing purposes
+    - it returns the generated embeddings per region and the corresponding uuid
+    - it is used in scenario_embeddings to generate embeddings-csvs
+
+    Args:
+        base_dataset_path (str): path to base image folder
+        region_dataset_path (str): path to folder where normalized images (region images) lie/ should be saved
+        csv_folder_path (str): path to folder where metadata-csv lies and embeddings-csvs get saved
+        model (DenseNet | ResNet): CNN-model that generates the embeddings, uses default-Model
+        normalize (bool): choose whether or not to perform hand-normalization. Defaults to True. False skips normalization-step (e.g. for tests or in case the dataset was already in use and embeddings need to be generated anew)
+        save_images (bool): choose whether or not to save the normalized images (hand-regions). Defaults to True. False skips saving step (e.g. for tests)
+        save_csvs (bool): choose whether or not to save the embeddings in csv-files. Defaults to True. False skips the setup of the csvs and the saving of the embeddings in the csv-files.
+    """
     # TODO: docstring
     ######## STEP 1: Hand normalization #################################
     if normalize:
