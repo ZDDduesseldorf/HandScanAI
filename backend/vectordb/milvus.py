cfd395a4f2e22828a39ed979bef64e004fb9e23b
Marina Künzig <marina.kuenzig@study.hs-duesseldorf.de>
2025-02-16 22:26:20 +0100

    refactor milvus params for easier changes

--- backend/vectordb/milvus.py
+++ backend/vectordb/milvus.py
@@ -17,8 +17,16 @@ milvus_collection_name = "hand_regions"
 
 milvus_default_top_k = 5
 
+milvus_metric_type = "L2"
+
+milvus_index_params = {
+    "index_type": "IVF_FLAT",
+    "metric_type": milvus_metric_type,
+    "params": {"nlist": 128},
+}
+
 milvus_default_search_params = {
-    "metric_type": "L2",  # Gleiche Metrik wie beim Index
+    "metric_type": milvus_metric_type,  # Gleiche Metrik wie beim Index
     "params": {"nprobe": 10},  # Anzahl der durchsuchten Cluster (abhängig von nlist)
 }
 
@@ -79,13 +87,8 @@ def create_miluvs_collection(collection_name: str) -> None:
                 collection.create_partition(partition_name=partition_name)
                 print(f"Partition '{partition_name}' successfully created.")
 
-        index_params = {
-            "index_type": "IVF_FLAT",
-            "metric_type": "L2",
-            "params": {"nlist": 128},
-        }
         if not collection.has_index():
-            collection.create_index(field_name="vector", index_params=index_params)
+            collection.create_index(field_name="vector", index_params=milvus_index_params)
     else:
         collection = Collection(name=collection_name)
         print(f"Collection '{collection_name}' exists.")
