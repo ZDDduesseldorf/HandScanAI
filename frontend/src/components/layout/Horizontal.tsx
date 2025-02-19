643586c452eb5aea4dae1fc4e690ab69c59ca818
Cynthia <cynthia.voss98@gmail.com>
2025-02-19 20:51:15 +0100

    Add new content to explanation page

--- frontend/src/components/layout/Horizontal.tsx
+++ frontend/src/components/layout/Horizontal.tsx
@@ -2,13 +2,15 @@ import { Box, styled } from '@mui/material';
 
 interface Props {
   children: React.ReactNode;
+  margin?: string;
 }
 
-export default function Horizontal({ children }: Props) {
+export default function Horizontal({ children, margin }: Props) {
   const Horizontal = styled(Box)`
     display: flex;
     flex-direction: row;
     gap: 10px;
+    margin: ${margin ? margin : "auto"};
   `;
 
   return <Horizontal>{children}</Horizontal>;
