250eb9036a70cc934532f8faeb64ac9644720027
Marina Künzig <marina.kuenzig@study.hs-duesseldorf.de>
2025-02-19 12:03:27 +0100

    automatic creation of logs folder and csv files, added date to folder

--- backend/utils/logging_utils.py
+++ backend/utils/logging_utils.py
@@ -1,25 +1,29 @@
 from pathlib import Path
+from datetime import datetime
 
 from utils.key_enums import PipelineDictKeys as Keys
 from utils.key_enums import PipelineAPIKeys as APIKeys
-from utils.csv_utils import create_csv_with_header, add_entry_to_csv
+from utils.csv_utils import create_csv_with_header, add_entry_to_csv, check_or_create_folder, check_file_exists
 # logger für uuid, datum, uhrzeit
 # rest csvs
 
 
 def logging_manager():
     temp_base_dir = Path(__file__).resolve().parent.parent
-    path_to_logs = temp_base_dir / "logs"
 
+    date = datetime.now()
+    formatted_date = date.strftime("%Y-%m-%d")
+
+    path_to_logs = temp_base_dir / "logs" / formatted_date
     path_to_nearest_neighbours = path_to_logs / "nearest_neighbours.csv"
     path_to_classification = path_to_logs / "classification.csv"
     path_to_input_data = path_to_logs / "input_data.csv"
 
-    return path_to_nearest_neighbours, path_to_classification, path_to_input_data
+    return path_to_logs, path_to_nearest_neighbours, path_to_classification, path_to_input_data
 
 
 def setup_csv_logging():
-    path_to_nearest_neighbours, path_to_classification, path_to_input_data = logging_manager()
+    path_to_logs, path_to_nearest_neighbours, path_to_classification, path_to_input_data = logging_manager()
 
     header_nearest_neigbour = [
         Keys.UUID.value,
@@ -43,13 +47,20 @@ def setup_csv_logging():
 
     header_input_data = [APIKeys.UUID.value, APIKeys.REAL_AGE.value, APIKeys.REAL_GENDER.value]
 
-    create_csv_with_header(path_to_nearest_neighbours, header_nearest_neigbour)
-    create_csv_with_header(path_to_classification, header_classification)
-    create_csv_with_header(path_to_input_data, header_input_data)
+    check_or_create_folder(path_to_logs)
+
+    if not check_file_exists(path_to_nearest_neighbours):
+        create_csv_with_header(path_to_nearest_neighbours, header_nearest_neigbour)
+
+    if not check_file_exists(path_to_classification):
+        create_csv_with_header(path_to_classification, header_classification)
+
+    if not check_file_exists(path_to_input_data):
+        create_csv_with_header(path_to_input_data, header_input_data)
 
 
 def logging_nearest_neighbours(uuid, dict_all_info_knn: dict):
-    path_to_nearest_neighbours, _, _ = logging_manager()
+    _, path_to_nearest_neighbours, _, _ = logging_manager()
     save_nearest_neighbours_info(uuid, dict_all_info_knn, path_to_nearest_neighbours)
 
 
@@ -68,7 +79,7 @@ def save_nearest_neighbours_info(uuid, dict_all_info_knn: dict, path_to_nearest_
 
 
 def logging_classification(uuid, age_dict, gender_dict, ensemble_df):
-    _, path_to_classification, _ = logging_manager()
+    _, _, path_to_classification, _ = logging_manager()
     save_classifiaction_info(uuid, age_dict, gender_dict, ensemble_df, path_to_classification)
 
 
@@ -101,7 +112,7 @@ def save_classifiaction_info(uuid, age_dict, gender_dict, ensemble_df, path_to_c
 
 
 def logging_input_data(uuid, ground_truth_data: dict):
-    _, _, path_to_input_data = logging_manager()
+    _, _, _, path_to_input_data = logging_manager()
     save_input_data_info(uuid, ground_truth_data, path_to_input_data)
 
 
