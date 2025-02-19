f7aa8a39ca4a71b00b11ce64e6c4fe9709162da6
Cynthia <cynthia.voss98@gmail.com>
2025-02-19 20:06:11 +0100

    Style processing page

--- frontend/src/components/layout/Centered.tsx
+++ frontend/src/components/layout/Centered.tsx
@@ -2,9 +2,10 @@ import { Box, styled } from '@mui/material';
 
 interface Props {
   children: React.ReactNode;
+  style?: string;
 }
 
-export default function Centered({ children }: Props) {
+export default function Centered({ children, style }: Props) {
   const Centered = styled(Box)`
     display: flex;
     flex-direction: column;
@@ -12,6 +13,7 @@ export default function Centered({ children }: Props) {
     width: 90%; /* Expands while keeping margins */
     max-width: 1000px; /* Prevents it from being too narrow */
     margin: 0 auto;
+    ${style}
   `;
 
   return <Centered>{children}</Centered>;
