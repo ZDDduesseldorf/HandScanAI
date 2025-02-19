5f0de17d64851c43c2d36f797e503f54b0537355
Cynthia <cynthia.voss98@gmail.com>
2025-02-19 22:37:18 +0100

    Fix layout on privacy page and add documentation

--- frontend/src/pages/PrivacyNotice.tsx
+++ frontend/src/pages/PrivacyNotice.tsx
@@ -1,21 +1,34 @@
+//external imports
 import { useNavigate } from 'react-router-dom';
 
+//component imports
 import Header from '@/components/custom/Header';
 import TextCard from '@/components/cards/WithText';
 import NarrowBottomSticky from '@/components/buttons/NarrowFixedBottomRight';
 import Centered from '@/components/layout/Centered';
+import WithMargins from '@/components/layout/WithMargins';
 
+/**
+ * Shows the user a short text informing them about data privacy issues. 
+ * A button can be used to navigate to the next page.
+ * 
+ * @returns Page showing a simple informative text
+ */
 export default function PrivacyNotice() {
+  /**
+   * Method for changing the location
+   * @see https://reactrouter.com/6.29.0/hooks/use-navigate
+   */
   const navigate = useNavigate();
 
   return (
-    <>
+    <WithMargins mx="2em" my="1.5em">
       <Header title="Bevor wir Beginnen..." />
       <Centered>
         <TextCard
           title="Daten statt Gebühren"
           text="Wie bei vielen digitalen Diensten gilt auch hier: Statt eines 
-          klassischen Preises zahlst du mit etwas anderem – deinen Daten. In 
+          klassischen Preises zahlst du mit etwas anderem - deinen Daten. In 
           unserer App sind es vor allem deine Interaktionen und die Bilder deiner 
           Hand, die die KI verarbeitet und lernen lassen. Diese Daten sind der 
           „Treibstoff“, der der KI hilft, intelligenter, präziser und 
@@ -27,6 +40,6 @@ export default function PrivacyNotice() {
       <NarrowBottomSticky onClick={() => navigate('/image-capture')}>
         Weiter
       </NarrowBottomSticky>
-    </>
+    </WithMargins>
   );
 }
