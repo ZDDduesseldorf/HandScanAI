0ab26c7bdf7d5c9f82dcee8eafabc6c92bc22aab
Cynthia <cynthia.voss98@gmail.com>
2025-02-20 11:22:14 +0100

    Add documentation to text components

--- frontend/src/components/text/Centered.tsx
+++ frontend/src/components/text/Centered.tsx
@@ -1,11 +1,24 @@
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 
+/**
+ * Defines the typescript types of the parameters
+ */
 interface Props {
-  children: string | React.ReactNode;
+  children: React.ReactNode;
 }
 
+/**
+ * Centers text and components that can be placed within a <p> element
+ *
+ * @param children Text or React components that can be placed within a <p>
+ *  element that is to be centered
+ * @returns Component that displays centered text
+ */
 export default function Centered({ children }: Props) {
+  /**
+   * Styling for a mui <p> component that centers text
+   */
   const Text = styled(Typography)`
     margin: 1em 0;
     font-family: 'Poppins', sans-serif;
