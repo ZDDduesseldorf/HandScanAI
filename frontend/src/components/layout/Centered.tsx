bdb0cf6ab9bba0e7e985c8432f89176a184771a9
Cynthia <cynthia.voss98@gmail.com>
2025-02-20 11:46:11 +0100

    Add documentation to layout components

--- frontend/src/components/layout/Centered.tsx
+++ frontend/src/components/layout/Centered.tsx
@@ -1,11 +1,27 @@
 import { Box, styled } from '@mui/material';
 
+/**
+ * Defines the typescript types of the parameters
+ */
 interface Props {
   children: React.ReactNode;
   style?: string;
 }
 
+/**
+ * Container that centers all elements passed to it in the middle
+ * of the screen. The elements are centered on both the x-axis and
+ * the y-axis.
+ *
+ * @param children React components that are placed on the container
+ * @param style Optional additional styles
+ * @returns Component that adds a flex container that centers content
+ */
 export default function Centered({ children, style }: Props) {
+  /**
+   * Styling for the mui <div> element that centers content on x- and
+   * y-axis with a flex box and adds optional custom styles.
+   */
   const Centered = styled(Box)`
     display: flex;
     flex-direction: column;
