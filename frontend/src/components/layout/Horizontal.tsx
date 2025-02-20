47b19a988869f4dc340a72640f9dd8657b62880d
Cynthia <cynthia.voss98@gmail.com>
2025-02-20 10:35:52 +0100

    Change styling of image post capture

--- frontend/src/components/layout/Horizontal.tsx
+++ frontend/src/components/layout/Horizontal.tsx
@@ -3,14 +3,16 @@ import { Box, styled } from '@mui/material';
 interface Props {
   children: React.ReactNode;
   margin?: string;
+  style?: string;
 }
 
-export default function Horizontal({ children, margin }: Props) {
+export default function Horizontal({ children, margin, style = '' }: Props) {
   const Horizontal = styled(Box)`
     display: flex;
     flex-direction: row;
     gap: 10px;
     margin: ${margin ? margin : '0'};
+    ${style};
   `;
 
   return <Horizontal>{children}</Horizontal>;
