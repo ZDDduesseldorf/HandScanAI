ab4f560b9589e56028574888eb8106b0f26ab660
Cynthia <cynthia.voss98@gmail.com>
2025-02-20 11:46:11 +0100

    Add documentation to layout components

--- frontend/src/components/layout/Vertical.tsx
+++ frontend/src/components/layout/Vertical.tsx
@@ -1,11 +1,26 @@
 import { Box, styled } from '@mui/material';
 
+/**
+ * Defines the typescript types of the parameters
+ */
 interface Props {
   children: React.ReactNode;
   style?: string;
 }
 
+/**
+ * Container that arranges all elements passed to it vertically one
+ * below the other.
+ *
+ * @param children React components that are placed on the container
+ * @param style Optional additional styles
+ * @returns Component that adds a flex container
+ */
 export default function Vertical({ children, style = '' }: Props) {
+  /**
+   * Styling for the mui <div> element that adds a flexbox with vertical
+   * ordering and adds optional custom styles.
+   */
   const Vertical = styled(Box)`
     display: flex;
     flex-direction: column;
