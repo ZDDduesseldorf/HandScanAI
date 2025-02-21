e00ab8d1c9999e0596d34a0947414173e3ecd536
Marina Künzig <marina.kuenzig@study.hs-duesseldorf.de>
2025-02-21 19:15:31 +0100

    update random forest scenario docstrings

--- backend/tests/scenarios/random_forest_feature_importance_scenario.py
+++ backend/tests/scenarios/random_forest_feature_importance_scenario.py
@@ -1,6 +1,6 @@
 import pandas as pd
+from pathlib import Path
 
-# from pathlib import Path
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.inspection import permutation_importance
@@ -10,7 +10,17 @@ from utils.key_enums import PipelineDictKeys as Keys
 from utils.key_enums import HandRegions
 
 
-# TODO: zum ausführen der feature_importance berechnung
+"""
+Prerequisites:
+For this scenario, you'll need the classification data for age and gender in form of csv-files (see file-paths below).
+They are created by running the classifier_scenario beforehand. The data from the ensemble-classification is not needed here.
+
+To run this scenario, comment in the function test_calculate_feature_importances below
+and run it on the console from the /backend-folder via 'pytest -s tests/scenarios/random_forest_feature_importance_scenario.py'.
+
+The results are printed to the commandline and you can copy them from there to analyze them.
+"""
+# TODO: comment in to run the feature importance calculation
 """def test_calculate_feature_importances():
     temp_base_dir = Path(__file__).resolve().parent
     path_to_age_csv = temp_base_dir / "result_csvs" / "classifier" / "classification_data_age.csv"
@@ -19,40 +29,46 @@ from utils.key_enums import HandRegions
     calculate_feature_importances(path_to_age_csv, path_to_gender_csv)"""
 
 
-def calculate_feature_importances(path_to_age_csv, path_to_gender_csv):
+def calculate_feature_importances(path_to_age_csv: Path, path_to_gender_csv: Path):
     """
     Calculates the feature importances for both the RandomforestClassifier (gender) and RandomForestRegressor (age) and prints them.
 
+    Calculated feature importances are Gini Importance, Permutation Importance and coefficients of linear regression models.
+
     Args:
-        path_to_age_csv (str): full path to csv with classification data from age region classifiers
-        path_to_gender_csv (str): full path to csv with classification data from gender region classifiers
+        path_to_age_csv (Path): absolute path to csv with classification data from age region classifiers
+        path_to_gender_csv (Path): absolute path to csv with classification data from gender region classifiers
     """
+    print()
     print("---AGE---")
     X_age, y_age = load_random_forest_data(path_to_age_csv)  # noqa: N806
     X_age_train, X_age_test, y_age_train, y_age_test = prepare_data_for_random_forest(X_age, y_age)  # noqa: N806
     forest_age = random_forest_regressor(X_age_train, y_age_train)
     calculate_feature_importance(forest_age, X_age_test, y_age_test)
 
+    print()
     print("---GENDER---")
     X_gender, y_gender = load_random_forest_data(path_to_gender_csv)  # noqa: N806
     X_gender_train, X_gender_test, y_gender_train, y_gender_test = prepare_data_for_random_forest(X_gender, y_gender)  # noqa: N806
     forest_gender = random_forest_classifier(X_gender_train, y_gender_train)
     calculate_feature_importance(forest_gender, X_gender_test, y_gender_test)
 
+    print()
     print("Linear Models")
     create_linear_models(X_age_train, y_age_train, X_gender_train, y_gender_train)
 
 
-def load_random_forest_data(path_to_csv: str):
+def load_random_forest_data(path_to_csv: Path):
     """
     Loads data from csv into Dataframe and does data and label split.
 
     Args:
-        path_to_csv (str): full path to csv with classification data
+        path_to_csv (Path): absolute path to csv with classification data
 
     Returns:
-        X (pd.DataFrame): data containing mean classification results per region
-        y (pd.DataFrame): label
+        (X, y) (pd.DataFrame, pd.DataFrame):
+        - X: data containing classification results per region
+        - y: label (age or gender)
     """
     data_raw = pd.read_csv(path_to_csv)
 
@@ -73,7 +89,7 @@ def prepare_data_for_random_forest(X: pd.DataFrame, y: pd.DataFrame):  # noqa: N
         y (pd.DataFrame): dataframe holding the corresponding labels
 
     Returns:
-        X_train, X_test, y_train, y_test: train-test-split data and label
+        (X_train, X_test, y_train, y_test) (pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame): train-test-split data and label
     """
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)  # noqa: N806
     return X_train, X_test, y_train, y_test
@@ -88,7 +104,7 @@ def random_forest_regressor(X_train: pd.DataFrame, y_train: pd.DataFrame) -> Ran
         y_train (pd.DataFrame): corresponding labels
 
     Returns:
-        forest (RandomForestRegressor): best randomforestregressor for the given training data
+        forest (RandomForestRegressor): best RandomForestRegressor for the given training data
     """
 
     forest = RandomForestRegressor()
@@ -112,7 +128,7 @@ def random_forest_classifier(X_train: pd.DataFrame, y_train: pd.DataFrame) -> Ra
         y_train (pd.DataFrame): corresponding labels
 
     Returns:
-        forest (RandomForestRegressor): best randomforestregressor for the given training data
+        forest (RandomForestRegressor): best RandomForestClassifier for the given training data
     """
 
     forest = RandomForestClassifier()
@@ -171,6 +187,7 @@ def create_linear_models(
 ):
     """
     Creates two linear models for the age- and gender-classification and prints their coefficients. Can be interpreted or used as weights.
+    Results were not prioritized as high as the RandomForest-Importances.
 
     Args:
         X_age_train (pd.DataFrame): training data from the train-test-split für age classification
@@ -180,8 +197,8 @@ def create_linear_models(
     """
     linear_model = LinearRegression()
     linear_model.fit(X_age_train, y_age_train)
-    print(f"linear model coefs: {linear_model.coef_}")
+    print(f"linear model coefs (age): \n{linear_model.coef_}")
 
     logistic_model = LogisticRegression()
     logistic_model.fit(X_gender_train, y_gender_train)
-    print(f"logistic model coefs: {logistic_model.coef_}")
+    print(f"logistic model coefs (gender): \n{logistic_model.coef_}")
